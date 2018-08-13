cb24613ee355c5cd80dd44a4998b212f79deda98
t-kurihara <takuya.kurihara00@gmail.com>
2018-08-13 18:02:15 +0900

    [add] Index page.

---
+++ src/components/Index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="root">
+      <vue-header/>
+    </div>
+</template>
+
+<script>
+import VueHeader from './Header'
+
+export default {
+  name: 'Index',
+  components: {
+    VueHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
