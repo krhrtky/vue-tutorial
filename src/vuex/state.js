0de9dbd2ba89b102f35bac758e1009de0b7fdc10
t-kurihara <takuya.kurihara00@gmail.com>
2018-08-14 23:03:06 +0900

    [add] vuex state file.

---
+++ src/vuex/state.js
@@ -0,0 +1,16 @@
+import Vue from 'Vue'
+import Vuex from 'Vuex'
+
+Vue.use(Vuex)
+
+const state = {}
+const actions = {}
+const getters = {}
+const mutations = {}
+
+export default new Vuex.Store({
+  state,
+  getters,
+  actions,
+  mutations
+})
