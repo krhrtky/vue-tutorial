b3f116c8077babfbebf488a21cf727da0dee51a1
krhrtky <takuya.kurihara00@gmail.com>
2018-08-15 12:33:39 +0900

    [change] Search add event.

--- src/components/page/Search.vue
+++ src/components/page/Search.vue
@@ -1,13 +1,32 @@
 <template>
   <div class="mdl-textfield mdl-js-textfield">
-    <input class="mdl-textfield__input" type="text" id="sample1">
-    <i class="material-icons">search</i>
+    <input
+      @change="CHANGE_KEYWORD"
+      class="mdl-textfield__input"
+      type="text"
+      id="sample1"
+    >
+    <i
+      @click="SEARCH"
+      class="material-icons"
+    >
+      search
+    </i>
   </div>
 </template>
 
 <script>
+import { mapActions } from 'vuex'
+import { CHANGE_KEYWORD, SEARCH } from '../../state/mutationTypes'
+
 export default {
-  name: 'Search'
+  name: 'Search',
+  methods: {
+    ...mapActions({
+      CHANGE_KEYWORD,
+      SEARCH
+    })
+  }
 }
 </script>
 
